2d4c9b92112bdebc6cf5cb33d4a544286e447c2d
Yael Avramsky <y0583296331@gmail.com>
2026-01-30 10:36:13 +0200

    feat: add DataLoader service for CSV reading

--- CertificationProcessor/Services/DataLoader.cs
+++ CertificationProcessor/Services/DataLoader.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CertificationProcessor.Domain;
+using CsvHelper.Configuration;
+using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,4 +12,31 @@ namespace CertificationProcessor.Services;
 
 public class DataLoader
 {
+    private readonly string _filePath;
+
+    public DataLoader(string filePath)
+    {
+        _filePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
+    }
+
+    public IEnumerable<CandidateCsvRecord> LoadCandidates()
+    {
+        if (!File.Exists(_filePath))
+            throw new FileNotFoundException($"CSV file not found: {_filePath}");
+
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            IgnoreBlankLines = true,
+            TrimOptions = TrimOptions.Trim,
+            MissingFieldFound = null
+        };
+
+        using var reader = new StreamReader(_filePath);
+        using var csv = new CsvReader(reader, config);
+
+        var records = csv.GetRecords<CandidateCsvRecord>().ToList();
+
+        return records;
+    }
 }
